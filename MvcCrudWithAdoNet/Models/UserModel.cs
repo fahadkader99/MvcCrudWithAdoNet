2319713d6fbe90f8f194364766db8cfdea08c873
Fahad Kader <fahadkader786@outlook.com>
2024-06-17 21:59:43 -0400

    New cs file added

--- MvcCrudWithAdoNet/Models/UserModel.cs
+++ MvcCrudWithAdoNet/Models/UserModel.cs
@@ -7,5 +7,12 @@ namespace MvcCrudWithAdoNet.Models
 {
     public class UserModel
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
     }
 }
+
+
+
---
+++ MvcCrudWithAdoNet/Service/UserDAL.cs
@@ -0,0 +1,20 @@
+﻿using MvcCrudWithAdoNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace MvcCrudWithAdoNet.Service
+{
+    public class UserDAL
+    {
+        SqlConnection _connection = new SqlConnection(ConfigurationManager.ConnectionStrings["cs"].ConnectionString);
+
+        public List<UserModel> GetUsers()
+        {
+
+        }
+    }
+}
